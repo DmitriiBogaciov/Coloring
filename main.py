a19c6d5ff33664358a63d784ceebe2fb59378ebf
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2023-03-03 12:02:55 +0100

    added functions iscoloring and color

--- main.py
+++ main.py
@@ -1,23 +1,74 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 
-Gb = nx.barabasi_albert_graph(20, 5)
-colormus = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+nodes = 50
+edges = 3
+steps = 1000
+
+Gb = nx.barabasi_albert_graph(nodes, edges)
+colormus = list(range(1, nodes + 1))
 rng = np.random.default_rng(12345)  # seed
 
 
-# bere na vstupu pole barev vrcholu poporade, cislum priradi nahodne barvy a vykresli graf
+def iscoloring(G, col):
+    for u, v in G.edges():
+        if col[u] == col[v]:
+            return False
+    return True
+
+
+def color(G, k, steps):
+    col = [0] * G.number_of_nodes()
+    bestcol = col[:]
+    bestscore = 0
+
+    # Random initialization
+    for i in range(G.number_of_nodes()):
+        col[i] = rng.integers(k)
+    score = sum([iscoloring(G, col)])
+    if score > bestscore:
+        bestcol = col[:]
+        bestscore = score
+
+    for i in range(steps):
+        # Random walk
+        rnode = rng.integers(G.number_of_nodes())
+        rcolor = rng.integers(k)
+        colnew = col[:]
+        colnew[rnode] = rcolor
+        score = sum([iscoloring(G, colnew)])
+        if score > bestscore:
+            bestcol = colnew[:]
+            bestscore = score
+        # Hill climbing
+        for j in range(10):
+            rnode = rng.integers(G.number_of_nodes())
+            rcolor = rng.integers(k)
+            colnew = col[:]
+            colnew[rnode] = rcolor
+            score = sum([iscoloring(G, colnew)])
+            if score > bestscore:
+                bestcol = colnew[:]
+                bestscore = score
+            if score >= sum([iscoloring(G, col)]):
+                col = colnew[:]
+                break
+    return bestcol, bestscore == G.number_of_nodes()
+
+
 def plot(G, cols):
     k = np.max(cols)
     symbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-    colmap = ["#" + ''.join(rng.choice(symbols, 6)) for i in range(k + 1)]
+    colmap = ["#" + ''.join(rng.choice(symbols, 6)) for _ in range(k + 1)]
 
     colors = [colmap[c] for c in cols]
 
+    exam = iscoloring(Gb, colors)
+    print(exam)
     nx.draw(G, node_color=colors, with_labels=True)
     plt.show()
 
 
 plot(Gb, colormus)
+plt.pause(0.001)
